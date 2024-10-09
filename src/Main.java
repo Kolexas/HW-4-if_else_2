9204f643313783f2d06b836819dee2a378ff5508
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-10-10 00:09:46 +0300

    Homework if_else_2. First edition.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
+        // Homework №4 Задание 1
         int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("1. Установите версию для приложения для iOS по ссылке");
